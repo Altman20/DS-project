21bcfc51650814ded42e9fba0accc1c02266a631
Altman20 <70535952+Altman20@users.noreply.github.com>
2020-10-13 22:32:44 -0400

    still garbage. Just different garbage. POST php populated

--- app/public/js/members.app.js
+++ app/public/js/members.app.js
@@ -1,55 +1,45 @@
   var members = new Vue({
       el: '#membersApp',
       data: {
-          members: [],
-            member: {}
+        memberList: [],
+        newMemberForm: {}
 
 },
 
 methods: {
-    fetchMembers() {
-      fetch('api/firefighters/index.php')
-      .then(response => response.json())
-      .then(json => { membersApp.members = json })
-    },
-    handleSubmit() {
-      fetch('api/firefighters/post.php', {
-        method: 'POST',
-        body: JSON.stringify(this.member),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      })
-      window.alert("Member was created");
-      window.location.href = 'members.html';
-    },
-    handleClick(m) {
-      fetch('api/membersWithCertification/index.php', {
-        method: 'POST',
-        body: JSON.stringify(m),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      }).then(response => response.json())
-        .then(json => { memberDetailApp.memberCert = json })
-      memberDetailApp.member = m;
-    },
-    handleEdit(m) {
-      window.location.href = 'editmember.html?memberGuid='+m.memberGuid;
-    },
-    handleDelete(m) {
-      fetch('api/firefighters/delete.php', {
-        method: 'POST',
-        body: JSON.stringify(m),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      })
-      window.alert("Member was deleted");
-      window.location.href = 'members.html';
+  newMemberData() {
+    return {
+      firstName:"",
+      lastName:"",
+      radio:"",
+      station:""
     }
   },
+  handleNewMemberForm(){
+    fetch('api/firefighters/post.php', {
+      method:'POST',
+      body: JSON.stringify(this.newMemberForm),
+      headers: {
+        "Content-Type": "applicaiton/json; charset=utf-8"
+      }
+    })
+    .then(response => response.json())
+    .then(json => {
+      console.log("returned from post:", json);
+      this.memberList.push(json[0]);
+    });
+    console.log("creating (POSTing)...!");
+    console.log(this.newMemberForm);
+    this.newMemberForm = this.newMemberData();
+  }
+  },
   created() {
-    this.fetchMembers();
+    fetch("api/firefighters/")
+    .then(response => response.json())
+    .then(json => {
+      this.memberList = json;
+      console.log(json)}
+    );
+    this.newMemberForm = this.newMemberData();
   }
 })
